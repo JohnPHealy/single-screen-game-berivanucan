a5304fdf2d967d4bf9f1027d1382998181ae5130
berivanucan <75083740+berivanucan@users.noreply.github.com>
2021-03-12 12:28:28 +0000

    Attack script is added, flying eye die animation is added with enemy health controller

---
+++ Magical Castle - Single Screen/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    int currentHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+        currentHealth -= damage;
+
+        if(currentHealth <=0 )
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("enemy died");
+        animator.SetBool("isDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(gameObject, 2);
+        this.enabled = false;
+        
+    }
+    
+}
+
+
---
+++ Magical Castle - Single Screen/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 40;
+    public float attackRate = 2f;
+    float nextAttackTime;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+       
+       
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
