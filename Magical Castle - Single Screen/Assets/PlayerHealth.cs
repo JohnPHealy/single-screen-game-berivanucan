a05a677430cd74641bf4c5162df8fa2227ba0815
berivanucan <75083740+berivanucan@users.noreply.github.com>
2021-03-13 10:35:46 +0000

    Health bar is added

---
+++ Magical Castle - Single Screen/Assets/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(1);
+        }
+        */
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enemy collide");
+        if(other.CompareTag("Enemy"))
+        {
+            Debug.Log("enemy tag");
+            TakeDamage(1);
+            Debug.Log("damage");
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
